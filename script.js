73d4ccc5b5f185cf1da45f2858ec8eff8fb33e29
rutra122 <90269415+rutra122@users.noreply.github.com>
2023-04-20 21:24:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const getJoke = async () => {
+
+    const randomNumber = Math.floor(Math.random() * 825)+1;
+    const apiUrl = `https://rickandmortyapi.com/api/character/${randomNumber}`;  
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const image = document.querySelector('#img');
+    const name = document.querySelector('#name');
+    const status = document.querySelector('#status');
+    const species = document.querySelector('#species');
+    const gender = document.getElementById("#render");
+
+    if (image) {
+        image.src = data.image;
+    }
+
+    name && (name.textContent = data.name);
+    status && (status.textContent = data.status);
+    species && (species.textContent = data.species);
+    gender && (gender.textContent = data.gender);
+  
+}
+
+const jokeButton = document.getElementById('get-joke');
+jokeButton.addEventListener('click', getJoke);
